533a18f26b90b7dba98576b13e0b2a17f2725b2a
suraj <s@s.com>
2024-03-02 17:19:58 +0545

    css added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import ProductDetailBySlug from './components/ProductDetailBySlug';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+  
+`;
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Product Listing</h1>
+      <h1 style={{marginLeft:'50px'}}><b>
+        <StyledLink to="/" className="product-listing-heading">
+          Product Listing
+        </StyledLink>
+        </b></h1>
+        {/* <h1>Product Listing</h1> */}
         <Routes>
           <Route exact path="/" element={<ProductList />} />
-          <Route path="/products/:slug" element={<ProductDetailBySlug/>} />
+          <Route path="/products/:slug" element={<ProductDetailBySlug />} />
         </Routes>
       </div>
     </Router>
--- src/components/ProductDetailBySlug.js
+++ src/components/ProductDetailBySlug.js
@@ -4,21 +4,22 @@ import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { Rating } from 'react-simple-star-rating';
 import Cssloder from './Cssloder';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
 const Acard = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  /* align-items: center; */
   padding: 20px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   border-radius: 5px;
   width: 100%;
-  /* max-width: 600px; */
   margin: 20px auto;
 
-  img {
-    width: 100%;
-    height: auto;
-    object-fit: cover;
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 0.5rem;
   }
 
   .slider {
@@ -27,6 +28,7 @@ const Acard = styled.div`
     scroll-snap-type: x mandatory;
     -ms-overflow-style: none;
     scrollbar-width: none;
+    justify-content: center;
 
     &::-webkit-scrollbar {
       display: none;
@@ -46,34 +48,56 @@ const Acard = styled.div`
     }
   }
 
-  .product-info {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    margin-bottom: 20px;
+  .price {
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #333;
+  }
 
-    .price {
-      font-size: 24px;
-      font-weight: bold;
-      color: #333;
-    }
-
-    .rating {
-      font-size: 18px;
-      color: #777;
-    }
+  .rating {
+    font-size: 1.125rem;
+    color: #777;
   }
 
   .description {
-    font-size: 16px;
+    font-size: 1rem;
     color: #333;
-    margin-bottom: 20px;
+    margin-bottom: 1rem;
   }
 `;
+
 const MainContainer = styled.div`
-display:flex;
-flex-direction:row;
-`
+  display: flex;
+  flex-direction: row;
+  box-shadow: 6px 4px 6px 4px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  width: '100%';
+`;
+
+const LeftContainer = styled.div`
+  margin-left: 10px;
+  margin-right: 50px;
+  width: 50%;
+  display: flex;
+  align-items: center;
+`;
+
+const RightContainer = styled.div`
+  width: 50%;
+`;
+const Button = styled.button`
+  margin:10px;
+  width:150px;
+  height:50px;
+  border-radius:5px;
+  border:none;
+  cursor: pointer;
+  &:hover {
+    /* background-color: blue; */
+    /* color: #fff; */
+    box-shadow: 0 0 10px #333;}
+  
+`;
 
 const ProductDetailBySlug = ({ location }) => {
   const [product, setProduct] = useState(null);
@@ -113,59 +137,76 @@ const ProductDetailBySlug = ({ location }) => {
     return <div>Loading...</div>;
   }
 
-
-
   return (
     <Acard>
-
       <Cssloder />
       <MainContainer>
-        <div>
-          <div className="w3-content" style={{ maxWidth: '1200px' }}>
+        <LeftContainer>
+          <div>
             {product.images.map((image, index) => (
               <img
                 key={index}
                 className={`mySlides ${index === slideIndex - 1 ? 'w3-opacity-off' : ''}`}
                 src={image}
-                style={{ minwidth: '70vh', height: '30vh', display: index === slideIndex - 1 ? 'block' : 'none' }}
+                style={{
+                 
+                  width:'750px',
+                  height: '400px',
+                  borderRadius: '10px',
+                  // backgroundColor:'red',
+                  padding:3,
+                 
+                  display: index === slideIndex - 1 ? 'block' : 'none'
+                }}
                 alt=""
               />
             ))}
 
-            <div className="w3-row-padding w3-section">
+            <div>
               {product.images.map((image, index) => (
-                <div key={index} className="w3-col s4">
+                <div
+                  key={index}
+                  className={`w3-col s4 ${index === slideIndex - 1 ? 'w3-opacity-off' : ''}`}
+                >
                   <img
-                    className={`demo w3-opacity w3-hover-opacity-off ${index === slideIndex - 1 ? 'w3-opacity-off' : ''}`}
                     src={image}
-                    style={{ width: '100%', height: '100px', cursor: 'pointer' }}
+                    style={{ width: '100%', height: '100px',padding:'3px', cursor: 'pointer' }}
                     onClick={() => currentDiv(index + 1)}
                   />
                 </div>
               ))}
             </div>
           </div>
-        </div>
-        <div>
-          <div className="product-info">
-            <div><h1>{product.title}</h1></div>
-
+        </LeftContainer>
+        <RightContainer>
+          <div>
+            <h1><b>{product.title}</b></h1>
+            <div className="description"><b>Description:</b>{product.description}</div>
+            <div className="rating">
+              <Rating
+                readonly={true}
+                initialValue={product.ratings}
+                size={24}
+                strokeWidth={2}
+                strokeColor="#ff9800"
+              />({product.totalRatings})
+            </div>
+            {product.offPercent > 0 && (
+            <p>
+              <span style={{ textDecoration: "line-through" }}>
+                Rs.{product.strikePrice}
+              </span>{" "}
+              ({product.offPercent}% off)
+            </p>
+          )}
+          
             <div className="price">Price: Rs.{product.price}</div>
 
-
-            <div className="rating">Rating: <Rating
-              readonly={true}
-              initialValue={product.ratings}
-              size={24}
-              strokeWidth={2}
-              strokeColor="#ff9800"
-            />({product.totalRatings})</div>
+            <div><b>Brand:</b> {product.brand.name}</div>
+            <Button style={{ marginTop: "10px", backgroundColor:'black',color:'white'}}><FontAwesomeIcon icon={faShoppingCart} style={{ color: "#fff" }} /> Add to Cart</Button>
+            <Button style={{ marginTop: "10px",backgroundColor:'lightgray' }}><FontAwesomeIcon icon={faHeart} /> Add to Wishlist</Button>
           </div>
-          </div>
-          <p>Brand: {product.brand.name}</p>
-
-
-          <p className="description">{product.description}</p>
+              </RightContainer>
       </MainContainer>
     </Acard>
   );
